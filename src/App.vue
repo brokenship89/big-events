8b816dc4815c5b27152a066fba2a3a78926b5e97
刘静瑶 <liujingyao@liujingyaodeMacBook-Pro.local>
2025-05-10 11:17:20 +0800

    导入elementplus

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
-<script setup></script>
+<script setup>
+import { useRoute, useRouter } from 'vue-router'
+const route = useRoute()
+const router = useRouter()
+const goList = () => {
+  router.push('/list')
+  console.log(route, router)
+}
+</script>
 
 <template>
   <div>我是app</div>
+  <el-button @click="$router.push('/home')">跳首页</el-button>
+  <el-button @click="goList">跳列表页</el-button>
 </template>
 
 <style scoped></style>
